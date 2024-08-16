ba9bc67eaa1618e2df9fda120774e86157d98b94
Prince Jain <102298253+PrinceMaloo@users.noreply.github.com>
2024-08-16 18:08:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ prompt="""You are Yotube video summarizer. You will be taking the transcript tex
 and summarizing the entire video and providing the important summary in points
 within 250 words. Please provide the summary of the text given here:  """
 
-genai.configure(api_key = "AIzaSyDFngW1i6lMFucchEDC6wEX29NX0o5XRuo")
+genai.configure(api_key = GOOGLE_API_KEY)
 
 def get_transcript(video_url):
   video_id = video_url.split("=")[-1]
